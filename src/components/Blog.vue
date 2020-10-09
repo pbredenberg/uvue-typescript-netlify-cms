611fb2af52b2ac46d857399da0c1f7fe3fdfdaf6
Paul Bredenberg <paulbredenberg@gmail.com>
2020-10-09 14:01:02 -0400

    feat: add blog page component

---
+++ src/components/Blog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="blog">
+    <article v-html="fileContents"></article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blog',
+  props: {
+    fileContents: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
